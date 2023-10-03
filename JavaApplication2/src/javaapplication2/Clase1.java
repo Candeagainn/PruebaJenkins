210f9a993141ac2c8be2b19809616833e823208b
candeagain <candeagainn@gmail.com>
2023-10-02 21:19:12 -0300

    New class added

---
+++ JavaApplication2/src/javaapplication2/Clase1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author cande
+ */
+public class Clase1 {
+    
+    private String nombre;
+    
+}
